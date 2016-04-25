83c7eabf77f7fdc467c9f6c6ff8a9cee2f0b472c
Yeiber Cano <yeibercano@gmail.com>
2016-04-25 16:16:54 -0700

    connects server to port

--- Server/server.js
+++ Server/server.js
@@ -9,4 +9,9 @@ var app = express();
 //creates default port
 var port = process.env.PORT || 4444;
 
+//connects app and port
+app.listen(port);
+//prints sucess when the server is running 
+console.log('Server now listening on por: ', port);
+
 
