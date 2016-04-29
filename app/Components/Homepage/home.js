59bd5a5c7911ec9372b4d09e1cb58c1ac4c68cab
Yeiber Cano <yeibercano@gmail.com>
2016-04-28 20:23:01 -0700

    adds send and featured methods to retrieve recipes

--- app/Components/Homepage/home.js
+++ app/Components/Homepage/home.js
@@ -1,2 +1,36 @@
 angular.module ('feastly.home', [])
-.controller('homeViewCtrl', function($scope) {});
+.controller('homeViewCtrl', function($scope, $http, $location) {
+
+  $scope.data = '';
+  $scope.featured = '';
+  $scope.query = '';
+
+  //on query make this call
+  $scope.send = function(){
+    console.log('send called');
+    return $http({
+          method: 'GET',
+          url: 'http://api.yummly.com/v1/api/recipes?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42', params: {'q':$scope.query}
+      }).then(function(res) {
+        console.log(res.data);
+        $scope.data = res.data;
+        $location.path('#/recipe');
+            // $scope.$apply();
+          return res.data;
+    });
+  };
+  //on load, features pages come in
+  $scope.featured = function(){
+    console.log('featured called');
+    return $http({
+          method: 'GET',
+          url: 'http://api.yummly.com/v1/api/recipes?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42&q=empanadas'
+      }).then(function(res) {
+        console.log(res.data);
+        $scope.featured = res.data;
+          return res.data;
+    });
+  };
+  //invokes featured() on load
+  $scope.featured();
+});
