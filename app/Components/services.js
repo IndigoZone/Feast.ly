aad9f8b78a3d5e1b3c1394730f3b8dd0f046d2c4
Yeiber Cano <yeibercano@gmail.com>
2016-05-02 16:26:39 -0700

    recipe view from search

--- app/Components/services.js
+++ app/Components/services.js
@@ -2,6 +2,7 @@ var recipeViewRes;
 angular.module('feastly.services', [])
 .factory('Recipe', function ($http) {
   var showRecipe = function(value){
+    console.log('value:', value)
     return $http({
       method: 'GET',
       url: 'http://api.yummly.com/v1/api/recipe/' + value + '?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42'
