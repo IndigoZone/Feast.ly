58790b8214e45a32d050933d8cfa451d482ee62b
Andy Nixon <andielane@yahoo.com>
2016-07-10 12:39:50 -0700

    Changing removing local host from url.

--- app/Components/LandingPage/landingServices.js
+++ app/Components/LandingPage/landingServices.js
@@ -3,16 +3,18 @@ angular.module('landing.services',[])
   var register = function(username,password){
     return $http({
       method: "POST",
-      url: "http://localhost:4444/api/register",
-      // url: "/api/register",
+      // for local testing use local host
+      // url: "http://localhost:4444/api/register",
+      url: "/api/register",
       data: {username: username, password: password}
     });
   };
   var login = function(username,password){
     return $http({
       method: "POST",
-      url: "http://localhost:4444/api/login",
-      // url: "/api/login",
+      // for local testing use local host endpoint
+      // url: "http://localhost:4444/api/login",
+      url: "/api/login",
       data: {username: username, password: password}
     }).then(function(res){
       return res.data;
