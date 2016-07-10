046eacdad3c27cd55654b1f9c481295936e41c01
Andy Nixon <andielane@yahoo.com>
2016-07-10 13:13:02 -0700

    Fixing the Servers Access-Control-Allow-Origin header.

--- app/Components/services.js
+++ app/Components/services.js
@@ -5,7 +5,7 @@ angular.module('feastly.services', [])
     console.log('value:', value)
     return $http({
       method: 'GET',
-      url: 'http://api.yummly.com/v1/api/recipe/' + value + '?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42'
+      url: 'https://api.yummly.com/v1/api/recipe/' + value + '?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42'
     }).then(function(res){
       console.log(res.data)
       recipeViewRes = res.data;
